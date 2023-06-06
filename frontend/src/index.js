1a8ac0bb997d1fc605a9d5eac351893f21662e5f
nkengderick <133006400+nkengderick@users.noreply.github.com>
2023-06-06 16:44:12 +0100

    deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { MdDisabledByDevTools } from '@fvilers/disable-react-devtools';
 
-if (process.env.NODE_ENV)
-
 import {
   createBrowserRouter,
   RouterProvider,
